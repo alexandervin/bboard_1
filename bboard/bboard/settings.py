2f611ace1191265e478c49b937a3ecad22d3ed04
alexandr.vinitsky <alexandr.vinitsky@gmail.com>
2021-06-17 16:51:57 +0300

    commit

--- bboard/bboard/settings.py
+++ bboard/bboard/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-8+t-i6q9mc4)rour&)&_1(fb6!nwwj=z_ucvz@8ruhje*l+h#4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
