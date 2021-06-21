e27dd822c70c74abb1118c355eb5f7fe7bc6b2ac
alexandr.vinitsky <alexandr.vinitsky@gmail.com>
2021-06-21 15:10:04 +0300

    add_LogOut_fix

--- bboard/main/views.py
+++ bboard/main/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.http import Http404, HttpResponse
 from django.shortcuts import render
 from django.template import TemplateDoesNotExist
@@ -21,6 +22,10 @@ def other_page(request, page):
 class BBLoginView(LoginView):
     template_name = 'main/login.html'
 
-@login_required
+#@login_required
 def profile(request):
     return render(request, 'main/profile.html')
+
+class BBLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'main/logout.html'
+
