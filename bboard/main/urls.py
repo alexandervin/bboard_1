bc6c233a20e11cb5ec5579d5cccb166e20d4d999
alexandr.vinitsky <alexandr.vinitsky@gmail.com>
2021-06-21 11:55:55 +0300

    add_profile

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, other_page, BBLoginView
+from .views import index, other_page, BBLoginView, profile
 
 app_name = 'main'
 urlpatterns = [
+    path('account/profile',profile, name='profile'),
     path('account/login', BBLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
--- bboard/main/views.py
+++ bboard/main/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from django.http import Http404, HttpResponse
 from django.shortcuts import render
@@ -20,3 +21,6 @@ def other_page(request, page):
 class BBLoginView(LoginView):
     template_name = 'main/login.html'
 
+@login_required
+def profile(request):
+    return render(request, 'main/profile.html')
