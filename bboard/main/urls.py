7e30ea0a3ff5a101edc598b4640b4cbf0ce22073
alexandr.vinitsky <alexandr.vinitsky@gmail.com>
2021-06-21 10:05:42 +0300

    fix_add_Login

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -3,7 +3,7 @@ from .views import index, other_page, BBLoginView
 
 app_name = 'main'
 urlpatterns = [
-    path('account/login', BBLoginView.as_view, name='login'),
+    path('account/login', BBLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
 ]
