6c86d709ea6506f3fc358e25243c381818ee9448
alexandr.vinitsky <alexandr.vinitsky@gmail.com>
2021-06-22 11:17:49 +0300

    add_change_user

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import index, other_page, BBLoginView, profile, BBLogoutView
+from .views import index, other_page, BBLoginView, profile, BBLogoutView, ChangeUserInfoView
 
 app_name = 'main'
 urlpatterns = [
     path('accounts/login/', BBLoginView.as_view(), name='login'),
     path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_chenge'),
     path('accounts/profile/', profile, name='profile'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
