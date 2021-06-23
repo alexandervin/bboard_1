cb630d2cde617f4752d7d010d8fbbd71e5f911e8
alexandr.vinitsky <alexandr.vinitsky@gmail.com>
2021-06-23 16:19:45 +0300

    fix_register_user

--- bboard/main/forms.py
+++ bboard/main/forms.py
@@ -5,6 +5,7 @@ from django.core.exceptions import ValidationError
 
 from .apps import user_registered
 
+
 class ChangeUserInfoForm(forms.ModelForm):
     email = forms.EmailField(required=True, label='Адрес электронной почты')
 
@@ -17,7 +18,8 @@ class RegisterUserForm(forms.ModelForm):
     email = forms.EmailField(required=True, label='Адрес электронной почты')
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput,
                                 help_text=password_validation.password_validators_help_text_html())
-    password2 = forms.CharField(label='Введите пароль (повторно)', widget=forms.PasswordInput,
+    password2 = forms.CharField(label='Введите пароль (повторно)',
+                                widget=forms.PasswordInput,
                                 help_text='Введите еще раз тот же самый пароль для проверки')
 
     def clean_password1(self):
@@ -30,8 +32,9 @@ class RegisterUserForm(forms.ModelForm):
         super().clean()
         password1 = self.cleaned_data['password1']
         password2 = self.cleaned_data['password2']
-        if password1 and password2 and password1 != password1:
-            errors = {'password2': ValidationError('Введенные пароли не совпадают', code='password_mismatch')}
+        if password1 and password2 and password1 != password2:
+            errors = {'password2': ValidationError(
+                'Введенные пароли не совпадают', code='password_mismatch')}
             raise ValidationError(errors)
 
     def save(self, commit=True):
@@ -41,7 +44,7 @@ class RegisterUserForm(forms.ModelForm):
         user.is_activated = False
         if commit:
             user.save()
-        user_registered.send(RegisterUserForm, instans=user)
+        user_registered.send(RegisterUserForm, instance=user)
         return user
 
     class Meta:
